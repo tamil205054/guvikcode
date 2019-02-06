5b0f858ad312cb081140e27ffe3b2bb689126f57
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-06 07:04:19 -0800

    Create java20.java

---
+++ java20.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;  
+ class  mul {  
+   public static void main(String[] args) {  
+       Scanner s = new Scanner(System.in);  
+        
+       int i=1,input = s.nextInt();  
+   while(i<5)
+   {
+       System.out.print((i*input)+" ");
+       i=i+1;
+   } 
+}
+}  
