4dd305e963ba454e58e3f22921660c75ae93a884
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-06 02:01:56 -0800

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class hello{
+
+    public static void main(String[] args) {
+int count=0;
+    	Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+       for(int i=0;i<n;i++)
+       {
+        System.out.println("Hello");
+       }
+        
+    }
+}
