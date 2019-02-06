1bc3eff65641ed52c5dd11b681e6e4798d583076
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-06 08:24:31 -0800

    Create java26.java

---
+++ java26.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+public class sort
+{
+      public static void main(String arg[])
+      {
+        int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+
+        }
+       Arrays.sort(a);
+       int t=n/2;
+       for(int i=0;i<n;i++)
+       {
+           if(i==t)
+           {
+               System.out.println(a[i]);
+           }
+       }
+
+      
+}
+}
