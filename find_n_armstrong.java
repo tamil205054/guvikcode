e5252301a3eee9af813fe515e3e8a1d8af12fd6f
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-06 06:41:39 -0800

    Create find_n_armstrong.java

---
+++ find_n_armstrong.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;  
+ class Armstrong {  
+   public static void main(String[] args) {  
+       Scanner s = new Scanner(System.in);  
+        
+       int start = s.nextInt();  
+        
+       int end = s.nextInt();   
+       for (int i = start; i <= end; i++) {  
+           if (isArmstrong(i)) {  
+               System.out.println(i+" ");  
+           }  
+       }  
+   }  
+   public static boolean isArmstrong(int number) {  
+        int  originalNumber, remainder, result = 0;
+        originalNumber = number;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+
+        if(result == number)
+        {
+            return true;
+        }
+       return false;  
+   }  
+}  
