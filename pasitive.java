181960d3fcafc01369acbbc8739a7fc7996ddf8c
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-05 22:06:18 -0800

    Update pasitive.java

--- pasitive.java
+++ pasitive.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 {
     public static void main(String[] args) 
     {
-        int number;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number you want to check:");
-        number = input.nextInt(); 
+        int number=3;
+        
         
         if(number > 0)
         {
