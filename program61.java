431d88a5f8c33d8fff32141ecd65cab5b324992a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-07 10:36:57 -0800

    Create program61.java

---
+++ program61.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class cut
+{
+	public static void main(String args[])
+	{
+		Scanner SC=new Scanner(System.in);
+		String str=SC.next();
+        int num=SC.nextInt();
+      
+            System.out.print(str.substring(0,num));
+	}
+}
