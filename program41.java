06ca325d2996f6d0a07d0a47087fabd0fde469c1
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-07 06:37:09 -0800

    Create program41.java

---
+++ program41.java
@@ -0,0 +1,16 @@
+ import java.util.*;
+class string{  
+public static void main(String args[])  
+{   
+Scanner input=new Scanner(System.in);
+String str=input.next();
+int count=input.nextInt();         
+ for(int i=0;i<count;i++) 
+ {    
+    
+  System.out.println(str);    
+      
+ }    
+  
+}
+} 
