4eedeebb138ede7587678a8d13cfc8329afbb22d
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-07 08:00:31 -0800

    Create program.java

---
+++ program.java
@@ -0,0 +1,38 @@
+import java.io.*; 
+ import java.util.*;
+class poweroftwo 
+{ 
+    static int check(int n) 
+    { 
+        if (n == 0) 
+        {
+            return 0; 
+        } 
+        else
+        {
+        while (n != 1) 
+        { 
+            if (n % 2 != 0)
+            { 
+                return 0;
+            } 
+            n = n / 2; 
+        } 
+        return 1;
+        } 
+    }   
+    public static void main(String args[]) 
+    { 
+        Scanner input =new Scanner(System.in);
+        int number=input.nextInt();
+        if (check(number)==1)
+        { 
+            System.out.println("Yes"); 
+        }
+        else
+        {
+            System.out.println("No"); 
+        }  
+    } 
+} 
+  
