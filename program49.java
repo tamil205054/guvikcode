71f4f6785a156f5a1a971fa206019dca3947d3eb
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-07 07:39:10 -0800

    Create program49.java

---
+++ program49.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sum
+{
+    public static void main(String[] args) 
+    {
+        int num1,num2;
+        Scanner s = new Scanner(System.in); 
+        num1 = s.nextInt();
+        num2 = s.nextInt();
+      
+        System.out.print(num1+num2);
+    }
+}
