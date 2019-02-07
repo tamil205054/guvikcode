98fc57669c5222d9bd2cf9a3908da0a5c9829975
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-07 07:24:01 -0800

    Create program46.java

---
+++ program46.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class next
+{
+	public static void main(String args[])
+	{
+		Scanner SC=new Scanner(System.in);
+		int num=SC.nextInt();
+        
+            System.out.print(num+1);
+	}
+}
