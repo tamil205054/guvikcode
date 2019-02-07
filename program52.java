8f9a4875a81d60a0eb8fac7fdfebb84f2e4afc3a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-07 08:52:15 -0800

    Create program52.java

---
+++ program52.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+ class Number 
+{
+    public static void main(String args[])
+    {
+        int m;
+        Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        int Number=m;
+      if(Number==1)
+      {
+          System.out.println("One");
+      }
+       else if(Number==2)
+      {
+          System.out.println("Two");
+      }else if(Number==3)
+      {
+          System.out.println("Three");
+      } else if(Number==4)
+      {
+          System.out.println("Four");
+      }else if(Number==5)
+      {
+          System.out.println("Five");
+      }else if(Number==6)
+      {
+          System.out.println("Six");
+      }else if(Number==7)
+      {
+          System.out.println("Seven");
+      }else if(Number==8)
+      {
+          System.out.println("Eight");
+      }else if(Number==9)
+      {
+          System.out.println("Nine");
+      } 
+      else
+      {
+          System.out.println("Ten");
+      }
+    }
+}
