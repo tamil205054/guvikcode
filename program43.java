c87fb812e62dc85477fc7e4c1df6d66f1743c75a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-07 07:06:49 -0800

    Create program43.java

---
+++ program43.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class concat
+{
+	public static void main(String args[])
+	{
+		Scanner SC=new Scanner(System.in);
+		String str1=SC.next().toLowerCase();
+		String str2=SC.next().toLowerCase();
+        System.out.println(str1+""+str2);
+	}
+}
