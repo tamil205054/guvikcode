c2caa427fd2e0b5f44efa0dbc453d217cff4ea5a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-06 17:15:16 -0800

    Create program31.java

---
+++ program31.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class stringlen 
+{
+public static void main(String[] args) 
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter a string");
+String str= sc.nextLine();
+int count=0;
+String array[]= str.split(" ",0);
+ 
+ for(int i=0;i<array.length;i++)
+ {
+     count+=array[i].length();
+   
+}
+System.out.println(count);
+}
+}
