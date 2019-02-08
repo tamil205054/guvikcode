9026c0e5825d1c607fde958bf13f66ba06ae51ea
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 06:48:02 -0800

    Create program78.java

---
+++ program78.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+class factors
+{
+   public static void main(String args[])
+   {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+      
+           if(num%13==0)
+           {
+               System.out.print("yes");
+           }
+           else
+           {
+                System.out.print("no");
+           }
+      
+        
+                 
+   }
+} 
