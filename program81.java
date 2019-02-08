b62e165a91213f8729c80838b9bab9b9d82683ba
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 08:32:11 -0800

    Update program81.java

--- program81.java
+++ program81.java
@@ -1,27 +1,23 @@
- import java.util.Scanner;
+import java.util.Scanner;
  class digit_count 
 {
     public static void main(String args[])
     {
     Scanner input =new Scanner(System.in);
-    int n1[] =new int[4],n2[]=new int[4],i; 
-	for(i=0;i<3;i++)
-	{
-        n1[i]=input.nextInt(); 
-        n2[i]=input.nextInt();
+    int n1,n2,i; 
+	 n1=input.nextInt(); 
+        n2=input.nextInt();
 
-	}
-    for(int j=0;j<3;j++){
-    if(n1[j]>n2[j])
+	 
+    if(n1>n2)
 	{ 
-        System.out.println(n1[j]-n2[j]);
+        System.out.println(n1-n2);
 	}
 	else
 	{ 
         
-        System.out.println(n2[j]-n1[j]);
+        System.out.println(n2-n1);
 	}
     }
  
-    }
 }
