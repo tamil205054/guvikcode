3ba3a3de34b3d70bdbd3e473954c84879ca4caaf
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 05:06:35 -0800

    Create program70.java

---
+++ program70.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class power
+{
+    public static void main(String args[])
+    {
+        
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+  System.out.println(m*2);
+    }
+}
