f5f00e92b7337b0b9c20eae54c8d0607833cde44
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 04:47:52 -0800

    Create program68.java

---
+++ program68.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ class div
+{
+    public static void main(String args[])
+    {
+        
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+    
+       
+            if(m%7==0)
+            {
+                System.out.println("yes");
+            }
+            else
+            {
+                System.out.println("no");
+            }
+    }
+}
