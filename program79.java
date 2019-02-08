21a27fb3d83e37384147ed927c710d0d609e2ef3
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 06:53:16 -0800

    Create program79.java

---
+++ program79.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+class square
+{
+   public static void main(String args[])
+   {
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        int prodect=num1*num2;
+           if(prodect % 2==0)
+           {
+               System.out.print("yes");
+           }
+           else
+           {
+                System.out.print("no");
+           }
+      
+        
+                 
+   }
+} 
