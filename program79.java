26452f940b9ae367007481ac8a874fea9f8d57a2
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 07:23:02 -0800

    Update program79.java

--- program79.java
+++ program79.java
@@ -7,8 +7,15 @@ class square
         Scanner in = new Scanner(System.in);
         int num1 = in.nextInt();
         int num2 = in.nextInt();
-        int prodect=num1*num2;
-           if(prodect % 2==0)
+        int prodect=num1*num2,flag=0;
+        for(int i=1;i<=Math.max(num1,num2);i++)
+        {
+            if(i*i==prodect)
+            {
+                flag=1;
+            }
+        }
+           if(flag==1)
            {
                System.out.print("yes");
            }
@@ -16,8 +23,5 @@ class square
            {
                 System.out.print("no");
            }
-      
-        
-                 
    }
 } 
