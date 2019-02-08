0c4ba53c4941a95fbb72ef8766e16432bd1c87f1
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 05:28:04 -0800

    Create program73.java

---
+++ program73.java
@@ -0,0 +1,32 @@
+import java.util.*;
+ 
+class find
+{
+   public static void main(String args[])
+   {
+      int num,start,end;
+      Scanner in = new Scanner(System.in);
+      num = in.nextInt();
+      start   = in.nextInt();
+      end = in.nextInt();
+     
+      int count=0;
+     
+      for (int i=start;i<end;i++)
+      {
+         
+         if(i==num)
+         {
+             count=1;
+         }
+      }  
+      if (count==1)
+      {
+            System.out.println("yes");
+      }
+      else
+      {
+          System.out.println("no");
+      } 
+   }
+} 
