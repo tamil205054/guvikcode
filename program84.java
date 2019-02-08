65eb2ca1f4278c4a50e388ad6776bc87809c5118
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 09:06:42 -0800

    Create program84.java

---
+++ program84.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class value { 
+  
+    public static void main(String[] args) 
+    { 
+        Scanner input=new Scanner(System.in);
+        char c =input.next().charAt(0); 
+        int ascii = c; 
+        System.out.println(ascii); 
+    } 
+} 
