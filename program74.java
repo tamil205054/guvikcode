a4ad3d7e9e1cfa88337a825777ce81ae5bebebdf
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 05:33:56 -0800

    Create program74.java

---
+++ program74.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+class round
+{
+   public static void main(String args[])
+   {
+      float num;
+      Scanner in = new Scanner(System.in);
+      num = in.nextFloat();
+    
+          System.out.println(Math.round(num)); 
+   }
+} 
