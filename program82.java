38efdfa459b8e2e4ab77522b658a964f3b4666c5
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 08:43:42 -0800

    Create program82.java

---
+++ program82.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ class digit_count 
+{
+    public static void main(String args[])
+    {
+    Scanner input =new Scanner(System.in);
+    float n1,n2;
+    double i; 
+	n1=input.nextFloat(); 
+    n2=input.nextFloat();
+
+	 i=n1*n2;
+     System.out.format("%.5f",i);
+    }
+ 
+}
