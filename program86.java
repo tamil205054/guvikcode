b207c1b071390eae88cf321ed2d3504e6a155a1f
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 09:31:07 -0800

    Create program86.java

---
+++ program86.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class value { 
+  
+    public static void main(String[] args) 
+    { 
+        Scanner input=new Scanner(System.in);
+        String c =input.next(); 
+        char ch[] = c.toCharArray(); 
+        int flag=0;
+        for(int i=0;i<ch.length;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(ch[j]==ch[i])
+                {
+                    flag=1;                     
+                }
+            }
+        
+        } 
+        if(flag==0)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        } 
+    } 
+
+} 
