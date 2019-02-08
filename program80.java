54c73a9a82db14cff0c53cc495f3d823c2c6178c
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 07:44:27 -0800

    Create program80.java

---
+++ program80.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ class digit_count 
+{
+    public static void main(String args[])
+    {
+        int m, n,temp,j=0,i=0;
+        Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        int Number=m,Count=0;
+        while(Number > 0) {
+			Number = Number / 10;
+			Count = Count + 1; 
+		}
+        int number[]= new int[Count] ;
+        while(m > 0)
+        {
+            n = m % 10;
+            m = m / 10;
+            number[j]=n;
+            j++;
+        }
+        for(i=number.length-1;i>=0;i--)
+        {
+            if(number[i]%2!=0)
+            {
+            System.out.print(number[i]+" ");
+            }
+        }
+    }
+}
