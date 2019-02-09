1aff0bc815b9d4ebf2fd0e4c4cc8c554aa430b05
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-09 07:53:54 -0800

    Update program101.java

--- program101.java
+++ program101.java
@@ -7,7 +7,8 @@ class prodect
         Scanner s = new Scanner(System.in);
         str = s.next();
         int n=s.nextInt();
-        int find=str.length()-n;
-        System.out.println(str.charAt(find));
+        int len=str.length();
+        int find=len-n;
+        System.out.println(str.substring(find,len));
     }
 }
