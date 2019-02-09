c0b2b3eb55e42db4d50e1fb3df22222dd911e2e7
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-09 08:38:10 -0800

    Create program105.java

---
+++ program105.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class string 
+{
+public static void main(String[] args) 
+{
+Scanner sc = new Scanner(System.in);
+ int d=sc.nextInt();
+ int p=sc.nextInt();
+ System.out.print(d+""+p);
+
+}
+}
