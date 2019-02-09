5be06d92b89e6e4e0efeee928435e5c56ac3d201
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-09 09:46:13 -0800

    Create player04.java

---
+++ player04.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+ class rev
+{
+    public static void main(String args[])
+    {    
+        Scanner sc=new Scanner(System.in);
+        String  n=sc.next();
+        StringBuffer s=new StringBuffer(n);
+        String m=".";
+        System.out.println(s.append(m));
+    }
+}
