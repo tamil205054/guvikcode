1af7130e8718aa8b3eb831aa39b781f13f1d0216
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-08 18:07:54 -0800

    Create program87.java

---
+++ program87.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.*;
+ 
+ class gcd
+{
+    public static void main(String args[])
+    {
+        int a,b,result=0;
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        
+        int n=Math.min(a,b);
+        for (int i=;i<=n;i++)
+        {
+            if(a%i==0 && b%i==0)
+            {
+                result=i;
+            }
+        }
+      
+        System.out.println(result);
+    }
+}
