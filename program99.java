68a07f1e360f184a9614503dc9d45fbe0b7afa08
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-09 07:26:28 -0800

    Create program99.java

---
+++ program99.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class find{
+	
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+        Scanner	sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+      
+        System.out.print((a*b)%c);
+	}
+}
