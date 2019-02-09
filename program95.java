87f50623dc00a28816c2523a65a48ca42cb2079d
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-09 05:52:35 -0800

    Create program95.java

---
+++ program95.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+class amount{
+	
+	public static void main(String[] args) 
+	{
+		int p,r,t;
+        Scanner	sc = new Scanner(System.in);
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        System.out.print((r*t*p)/100);
+       
+	}
+}
