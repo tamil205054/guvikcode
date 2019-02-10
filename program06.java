204e4188b4c9b4c4175f9dee9b81189e21dbfb90
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-10 07:43:39 -0800

    Create program06.java

---
+++ program06.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+ class Check {
+      public static void main(String[] args) {
+      int i,n,m=0,k=0;
+      String a,b;
+      Scanner scan=new Scanner(System.in); 
+      a=scan.nextLine(); 
+      b=scan.nextLine();
+      n=a.length();
+      for(i=0;i<n;i++)
+      {
+          for(int j=i+1;j<n;j++)
+      {
+          if(a.charAt(i) == a.charAt(j))
+          {
+              m=i;
+              k=j;
+          }
+      }
+    }
+
+      if(b.charAt(m)==b.charAt(k))
+      {
+          System.out.println("yes");
+      }
+      else
+      {
+          System.out.println("no");
+      }
+    }
+ 
+}
