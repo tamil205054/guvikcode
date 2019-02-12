c4df885536b3fed1b129cde48eaf53d3af842ae4
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-12 11:26:17 +0530

    Create player15.java

---
+++ player15.java
@@ -0,0 +1,30 @@
+import java.util.*;
+  class GFG  
+{ 
+      
+  
+    public static void main(String[] args) 
+    { 
+	Scanner input=new Scanner(System.in);
+        	String str =  input.next();
+        	int count[] = new int[256]; 
+        
+       	 int len = str.length(); 
+        	for (int i=0; i<len; i++) 
+	{	
+            		count[str.charAt(i)]++; 
+       	}
+        	int max = -1;   
+        	char result = ' ';    
+        
+        	for (int i = 0; i < len; i++) 
+	{ 
+            		if (max < count[str.charAt(i)]) 
+		{ 
+                			max = count[str.charAt(i)]; 
+                			result = str.charAt(i); 
+            		}	 
+        	}
+        	System.out.println(result); 
+    } 
+} 
