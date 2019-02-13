46dbf0377ed38cd793fad2c47bd9a3aaeb036357
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-13 07:39:41 -0800

    Create player28.java

---
+++ player28.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class remove
+{
+    public static void main(String arg[])
+    {
+        Scanner input=new Scanner(System.in);
+        String str=input.nextLine();
+        
+        String string[]=str.split(" ");
+        for(int i=0;i<string.length;i++)
+        {
+        System.out.print(string[i]);
+        }
+
+    }
+}
