74290b017aab5f5b3a0674b98efd219c824d345a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-13 07:34:05 -0800

    Create player26.java

---
+++ player26.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class remove
+{
+    public static void main(String arg[])
+    {
+        Scanner input=new Scanner(System.in);
+        String str=input.nextLine()         
+        System.out.print(str.replaceAll("\\s+", " "));
+
+    }
+}
