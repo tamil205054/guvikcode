ec96f18e16059aee5dfd65e004db774e2f125e0b
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-13 06:26:21 -0800

    Update player23.java

--- player23.java
+++ player23.java
@@ -5,12 +5,12 @@ class array{
         Scanner input =new Scanner(System.in);
         int n=input.nextInt();
         int ArrayofNumber[]=new int[n];
+        int insert=input.nextInt();
         for(int i=0;i<n;i++)
         {
             ArrayofNumber[i]=input.nextInt();
         }
         Arrays.sort(ArrayofNumber);
-        int insert=input.nextInt();
         for(int i=0;i<insert;i++)
         { 
             ArrayofNumber[i]=input.nextInt();
