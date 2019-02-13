1c6b1aaffcde7c55d26912ed47a469312cbc3efc
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-13 06:21:35 -0800

    Create player23.java

---
+++ player23.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class array{
+    public static void main(String arg[])
+    {
+        Scanner input =new Scanner(System.in);
+        int n=input.nextInt();
+        int ArrayofNumber[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            ArrayofNumber[i]=input.nextInt();
+        }
+        Arrays.sort(ArrayofNumber);
+        int insert=input.nextInt();
+        for(int i=0;i<insert;i++)
+        { 
+            ArrayofNumber[i]=input.nextInt();
+        }
+        Arrays.sort(ArrayofNumber);
+        for(int i=n-insert;i<n;i++)
+        {
+            System.out.printf("%d ",ArrayofNumber[i]);
+        }
+    }
+} 
