2e9d9f41a84fc70ec70000f645fbd312b733532a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-13 09:47:48 -0800

    Create player38.java

---
+++ player38.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class number
+{
+    public static void main(String arg[])
+    {
+        Scanner input=new Scanner(System.in);
+        int number=input.nextInt();
+        for(int i=2;i<number;i++)
+        {
+            if(number%i==0)
+            {
+                if(i%2==0)
+                {
+                    System.out.printf("%d ",i);
+                }
+            }
+        }
+    }
+}
