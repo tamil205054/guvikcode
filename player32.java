89cf9d759a8286eb2c3e3e57b1f9f6ca529a6fa1
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-13 08:21:44 -0800

    Create player32.java

---
+++ player32.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class find
+{
+    public static void main(String arg[])
+    {
+        Scanner input=new Scanner(System.in);
+          int n=input.nextInt();
+          int find =input.nextInt();
+          int arr[]=new int[n];
+          int count=0;  
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=input.nextInt();
+                if(arr[i]==find)
+                {
+                    count=1;
+                }
+            } 
+            if(count==1)
+            {
+                System.out.print("yes");
+            }
+            else
+            {
+                System.out.print("no");
+            }
+        
+    }
+}
