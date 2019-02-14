53e62acec4e68901019355cb94e0696c46e0fd3a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-14 09:06:45 -0800

    Create player51.java

---
+++ player51.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class triangle
+{
+    public static void main(String ar[])
+    {
+        Scanner input =new Scanner(System.in);
+        int number=input.nextInt(); 
+        int arr[]=new int[number];
+        for(int i=0;i<number;i++)
+        {
+            arr[i]=input.nextInt();
+        }
+        Arrays.sort(arr); 
+           System.out.print(arr[1]);
+       
+    }
+}
