d8c68e9345f57fcc5a2738e4ebc3e3a1f7c5d01d
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-14 05:34:28 -0800

    Create hunter03.java

---
+++ hunter03.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class number
+{
+    public static void main(String arg[])
+    {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int arrNum[]=new  int[n];
+        for(int i=0;i<n;i++)
+        {
+            arrNum[i]=input.nextInt();
+        }
+        for(int i=0; i<n ; i++)
+        {   
+          
+            if(arrNum[i]==i)
+            {
+                System.out.print(arrNum[i]);
+            }
+            if(i<n-1)
+            {
+                System.out.print(" ");
+            }
+            
+        }    
+    }
+}
