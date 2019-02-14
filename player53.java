7e3e0fa124e9cb2b5614cf712f64c0546ee3e51f
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-14 09:17:29 -0800

    Create player53.java

---
+++ player53.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class triangle
+{
+    public static void main(String ar[])
+    {
+        Scanner input =new Scanner(System.in);
+        String str=input.next(); 
+        char ch[]=str.toCharArray();
+        System.out.print(ch.length);
+       
+    }
+}
