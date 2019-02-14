fdd6370db36dceae595f96fcb9fbba27dbed8284
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-14 05:41:06 -0800

    Update hunter03.java

--- hunter03.java
+++ hunter03.java
@@ -10,18 +10,25 @@ class number
         {
             arrNum[i]=input.nextInt();
         }
+        int count=0;
         for(int i=0; i<n ; i++)
         {   
           
             if(arrNum[i]==i)
             {
                 System.out.print(arrNum[i]);
+                count++;
             }
             if(i<n-1&&arrNum[i]==i)
             {
                 System.out.print(" ");
             }
             
+        }
+        int num=-1;
+        if(count==0)
+        {
+            System.out.print(num);
         }    
     }
 }
