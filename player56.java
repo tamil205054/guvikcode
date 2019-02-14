5f858f80ff72bc0ccce4c9c81fb9d790f10ecb75
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-14 09:42:33 -0800

    Create player56.java

---
+++ player56.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class triangle
+{
+    public static void main(String ar[])
+    {
+        Scanner input =new Scanner(System.in);
+        String str=input.next(); 
+        char ch=input.next().charAt(0);
+        int count=0;
+        int len1=str.length();
+        
+         for(int i=0;i<len1 ;i++)
+         {
+             if(str.charAt(i)==ch)
+             {
+                 count=i+1;
+                 i=len1;
+             }
+         }     
+    
+             System.out.print(count);
+         
+    }
+}
