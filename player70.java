cb7ff4b0d138d4a68c4e5938dd98fabb10052424
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-15 07:55:58 -0800

    Create player70.java

---
+++ player70.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class arrays
+{
+    public static void main(String ar[])
+    {
+        Scanner input=new Scanner(System.in);
+        String n=input.next();
+         System.out.print(n+" Answer");
+ 
+        
+    }
+}
