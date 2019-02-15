eb59391977d22eea8c66f0fae97dc7b40cc9b819
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-15 08:04:56 -0800

    Create player74.java

---
+++ player74.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class arrays
+{
+    public static void main(String ar[])
+    {
+        Scanner input=new Scanner(System.in);
+        int a=input.nextInt();
+        int b=input.nextInt();
+        System.out.print(Math.round(Math.pow(a, b)));
+          
+    
+        
+    }
+}
