484ac21ab654bbe3103b42a9f03d82ab8a5b43f3
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-15 07:45:51 -0800

    Create player64.java

---
+++ player64.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class arrays
+{
+    public static void main(String ar[])
+    {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int k=input.nextInt();
+        int array[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            array[i]=input.nextInt();
+             
+        }
+        
+        Arrays.sort(array);
+        for (int i = 0; i < n; i++)  
+        { 
+              
+                if (array[i] < k)  
+                    {
+                    System.out.printf("%d ",array[i]);
+                    }
+            
+        } 
+    
+    
+        
+    }
+}
