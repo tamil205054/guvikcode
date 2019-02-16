cff1e76d8f92de0220c045ff309b3d91a9315cb5
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-16 02:06:36 -0800

    Create player91.java

---
+++ player91.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class binaryconverter
+	{
+		public static void main(String ar[])
+		{
+			Scanner input=new Scanner(System.in);
+			int num=input.nextInt();
+			System.out.print(Integer.toBinaryString(num));	
+		
+		}
+	}
