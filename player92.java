de0d4b5b46f6bb960db595a4846655fb72c2ba8a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-16 02:15:05 -0800

    Create player92.java

---
+++ player92.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class binaryconverter
+	{
+		public static void main(String ar[])
+		{
+			Scanner input=new Scanner(System.in);
+			String num=input.nextLine();
+			 
+		System.out.println(Integer.parseInt(num,2));
+		}
+	}     
