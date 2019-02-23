9b5ca406071fce16d0c5916578d7766fe6b05d3b
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-23 07:02:29 -0800

    Create player147.java

---
+++ player147.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class reverse
+{
+  public static void main(String[] args)
+  {
+      Scanner input=new Scanner(System.in);
+      String sentence=input.nextLine();
+      String str[]=sentence.split(" ");
+      int len=str.length;
+      ArrayList<String>arr=new ArrayList<String>();
+      for(int i=0;i<len;i++)
+      {
+        if(i!=0&&i!=len-1)
+        {
+          StringBuilder temp = new StringBuilder();
+          temp.append(str[i]);
+          temp=temp.reverse();
+          arr.add(temp.toString());
+        }
+        else
+        {
+          arr.add(str[i]);
+        }
+      }
+      for(int i=0;i<arr.size();i++)
+        {
+          System.out.print(arr.get(i));
+          if(i<arr.size()-1)
+          {
+            System.out.print(" ");
+          }
+        }
+  }
+}
