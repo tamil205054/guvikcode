4ba76b0f67b38615a8d7cb89e53c12d8436cf0f0
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-22 18:24:07 -0800

    Create player108.java

---
+++ player108.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class sumofarray
+{
+  public static void main(String[] args)
+  {
+      Scanner scan=new Scanner(System.in);
+      int n=scan.nextInt();
+      if(n<=100000)
+      {
+      int arrNum[]=new int[n];
+      for(int i=0;i<n;i++)
+      {
+        arrNum[i]=scan.nextInt();
+      }
+int sum;
+      for(int j=1;j<n;j++)
+      {
+       sum=0;
+
+        sum=arrNum[j-1]+arrNum[j];
+
+
+      arrNum[j]=sum;
+    }
+    for(int i=0;i<n;i++)
+    {
+      System.out.print(arrNum[i]);
+      if(i<n-1)
+      {
+        System.out.print(" ");
+      }
+    }
+
+  }
+  }
+}
