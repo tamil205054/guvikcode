243e7e4e9805cb5059be7fcfd6909458eefb040d
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-23 02:01:57 -0800

    Create player82.java

---
+++ player82.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class bitand
+{
+  public static void main(String[] args)
+  {
+      Scanner input=new Scanner(System.in);
+      int n=input.nextInt();
+      if(n<=100000)
+      {
+        int temp=0;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+
+          arr[i]=input.nextInt();
+          temp=arr[0];
+          temp=temp&arr[i];
+        }
+        System.out.print(temp);
+      }
+
+  }
+}
