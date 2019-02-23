076ec00e861fb0411ab25145941a50030b912361
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-22 22:39:40 -0800

    Create player44.java

---
+++ player44.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class substr
+{
+  public static void main(String[] args)
+  {
+      Scanner input=new Scanner(System.in);
+      String str=input.next();
+      int n=input.nextInt();
+      if(n<=str.length())
+      {
+      String str1=str.substring(0,n);
+      String str2=str.substring(n);
+      System.out.println(str2+str1);
+    }
+    else
+    {
+      System.out.print(str);
+    }
+  }
+}
