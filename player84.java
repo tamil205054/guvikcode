a6291238d487a1d8346f2c69ef744a7e8ee4a7f6
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-23 02:17:02 -0800

    Update player84.java

--- player84.java
+++ player84.java
@@ -7,16 +7,17 @@ class bitand
       int n=input.nextInt();
       if(n<=100000)
       {
+        int temp=0;
         int arr[]=new int[n];
-        ArrayList<Integer> bitwise=new ArrayList<Integer>();
         for(int i=0;i<n;i++)
         {
+
           arr[i]=input.nextInt();
-          bitwise.add(arr[i]|arr[i]);
+        
+          temp=temp|arr[i];
         }
-        Collections.sort(bitwise);
-         
-      System.out.print(bitwise.get(bitwise.size()-1));
+        System.out.print(temp);
       }
+
   }
 }
