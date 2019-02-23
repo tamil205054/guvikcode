156632d7cb1a17a3e3574ade5e577dc64914517b
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-23 02:18:30 -0800

    Create player85.java

---
+++ player85.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class not
+{
+  public static void main(String[] args)
+  {
+      Scanner input=new Scanner(System.in);
+      int n=input.nextInt();
+      if(n<=100000)
+      {
+
+      System.out.print(~n);
+      }
+  }
+}
