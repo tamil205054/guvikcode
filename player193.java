4cd489f73cb8b2c47a4fe2f80db08d81df7dd135
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-22 20:47:18 -0800

    Create player193.java

---
+++ player193.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class equation
+{
+  public static void main(String[] args)
+  {
+      Scanner input=new Scanner(System.in);
+      int a=input.nextInt(),b=input.nextInt(),x=input.nextInt();
+      System.out.print((a*x)+b);
+  }
+}
