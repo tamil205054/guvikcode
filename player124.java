551e56274c2c9abd164acce92d3af8653d33ecc3
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-23 03:53:50 -0800

    Create player124.java

---
+++ player124.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class smallest
+{
+  public static void main(String[] args)
+  {
+      Scanner in=new Scanner(System.in);
+      int n=in.nextInt();
+      if(n<=100000)
+      {
+        int arr1[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+          arr1[i]=in.nextInt();
+        }
+        int result=0;
+        for(int j=2;j<=100000;j++)
+        {
+          int count=0;
+          for(int i=0;i<n;i++)
+          {
+              if(j%arr1[i]==0)
+              {
+                  count++;
+              }
+              else
+              {
+                break;
+              }
+          }
+          if(count==n)
+          {
+            System.out.println(j);
+            break;
+          }
+        }
+      }
+  }
+}
