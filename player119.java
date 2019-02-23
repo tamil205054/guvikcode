f62c310fcfd1157054f26c93a8b3c9874e6c715f
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-02-23 04:28:51 -0800

    Create player119.java

---
+++ player119.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Decimal
+{
+        public static void main(String args[])
+        {
+            Scanner s=new Scanner(System.in);
+            String b=s.next();
+            System.out.println(Integer.parseInt(b,16));
+        }
+}
