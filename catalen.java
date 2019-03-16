1e8e6c6181d79e81b40814d7099aaaeafb0ff432
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-16 06:59:38 -0700

    Update catalen.java

--- catalen.java
+++ catalen.java
@@ -1,28 +1,38 @@
-  import java.util.*;
+import java.util.*;
 class Cataln { 
-	static int catalan(int n) { 
+	 int catalan(int n) { 
 		int result = 0;  
 		if (n <= 1) 
 		{ 
 			return 1; 
 		} 
+		else
+		{
 		for (int i = 0; i < n; i++) 
 		{ 
 			result = result+(catalan(i) * catalan(n - i - 1)); 
 		} 
+		}
 		return result; 
 	} 
 
 	public static void main(String[] args) {  
 		Scanner input=new Scanner(System.in);
+		 Cataln cn = new Cataln(); 
 		int n=input.nextInt(); 
+		if(n==0)
+		{
+			System.out.print(1);
+		}
+		else{
 		for (int i = 0; i < n; i++) 
 		{ 
-			System.out.print(catalan(i));
+			System.out.print(cn.catalan(i));
 				if(i<n-1)
 				{
 					System.out.print(" ");
 				}
+		}
 		}
 		input.close();
 	} 
