90c0d7ef54908eafa1d6aeb541fbd70901d0afb4
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-16 08:15:40 -0700

    Create Arraynumber.java

---
+++ Arraynumber.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class hexi
+{	
+    public static void main(String aa[])
+	{ 
+        Scanner ip=new Scanner(System.in);
+		int n=ip.nextInt();
+		int num[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			 num[i]=ip.nextInt();
+		}
+		List<Integer> result=new ArrayList<Integer>();
+		for(int i=0;i<n;i++)
+		{ 
+			if(i%2==0)
+			{
+				if(num[i]%2!=0)
+				{
+					result.add(num[i]);
+				}
+			} 
+			else
+			{
+				if(num[i]%2==0)
+				{
+					result.add(num[i]);
+				}
+			}
+		}
+		for(int i=0;i<result.size();i++)
+		{
+				System.out.print(result.get(i));
+			if(i<result.size()-1)
+			{
+				System.out.print(" ");
+			}
+		} 
+
+		ip.close();
+	}
+}
