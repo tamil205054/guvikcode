f4914419d85fcb1c4bd01d6ba5da34dc6472d9ff
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-15 22:58:14 -0700

    Create sumofDiagonal111.java

---
+++ sumofDiagonal111.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+class sumOfDiogonal
+{
+    public static void main(String[] args) 
+    {
+        Scanner ip=new Scanner(System.in);
+        int n=ip.nextInt();
+		int arrNum[][]=new int[n][n];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{	
+				arrNum[i][j]=ip.nextInt();
+			}
+		} 
+		int count=0;
+		for(int i=0;i<n;i++)
+		{ 
+			for(int j=0;j<n;j++)
+			{
+				if(i==j)
+				{
+					
+					count=arrNum[i][j]+count;
+				}
+			}
+		}
+		System.out.print(count);
+		ip.close();    
+    }
+}
