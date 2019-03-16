213e408fd7e95db191bdb96fda7e15354b831bbf
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-16 06:13:17 -0700

    Create rpsgame.java

---
+++ rpsgame.java
@@ -0,0 +1,311 @@
+import java.util.*;
+class sum
+{
+    public static void main(String aa[])
+	{ 
+        Scanner ip=new Scanner(System.in);  
+			char player_1=ip.next().toLowerCase().charAt(0);
+			char player_2=ip.next().toLowerCase().charAt(0);
+			if(player_1=='r'&&player_2=='s')
+			{
+				System.out.print('R');
+			}
+			else if(player_1=='p'&&player_2=='r')
+			{
+				System.out.print('P');
+				
+			}
+			else if(player_1=='s'&&player_2=='p')
+			{
+				System.out.print('S');
+				
+			}
+			else if(player_1=='s'&&player_2=='r')
+			{
+				System.out.print('R');
+			}
+			else if(player_1=='r'&&player_2=='p')
+			{
+				System.out.print('P');
+				
+			}
+			else if(player_1=='p'&&player_2=='s')
+			{
+				System.out.print('S');
+				
+			}
+			
+		ip.close();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
