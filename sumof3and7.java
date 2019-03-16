215710e308cb9526e73222ae6c14674ec3a89b99
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-16 05:45:01 -0700

    Create sumof3and7.java

---
+++ sumof3and7.java
@@ -0,0 +1,19 @@
+  import java.util.*;
+class sum
+{
+    public static void main(String aa[])
+	{ 
+        Scanner ip=new Scanner(System.in);  
+        int n=ip.nextInt();
+			if((n%3==0)||(n%7==0))
+			{
+				System.out.print("yes");
+			}
+			else
+			{
+				System.out.println("no");
+			}
+
+    }
+
+}
