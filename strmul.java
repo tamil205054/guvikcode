37dac660f2b32bfc82135f222984ef272e5e44cb
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-16 09:11:40 -0700

    Create strmul.java

---
+++ strmul.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class PowerOfSum{ 
+	 
+	public static void main(String[] args) {  
+		Scanner input=new Scanner(System.in);
+		String num1=input.next();
+		String num2=input.next();
+	 
+		System.out.print(Integer.parseInt(num1)*Integer.parseInt(num2));
+		input.close();
+		
+	} 
+} 
