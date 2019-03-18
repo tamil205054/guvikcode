3529d63fc47aedee23ac6eb5d8ef2b484d123011
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-18 07:58:06 -0700

    Create binaryindex.java

---
+++ binaryindex.java
@@ -0,0 +1,29 @@
+import java.util.*; 
+class Index
+{
+	public static void main (String[] args)
+	{
+		
+		Scanner input=new Scanner(System.in);
+	 
+		String  str=Integer.toBinaryString(input.nextInt());
+		  
+			 
+			 String str1="";
+			for(int j=str.length()-1;j>=0;j--)
+			{
+				 str1=str1+str.charAt(j);
+			}
+			for(int j=0;j<str1.length();j++)
+			{
+				if(str1.charAt(j)=='1')
+				{
+					System.out.print(j+1);
+					break;
+				}
+			}
+
+
+		input.close();
+	}
+}	
