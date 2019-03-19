a460243c7bd6950cc11e4f4bb20782bb66090616
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-18 19:31:25 -0700

    Create StringIndex.java

---
+++ StringIndex.java
@@ -0,0 +1,26 @@
+import java.util.*; 
+class StringIndex
+{
+	public static void main (String[] args)
+	{
+		Scanner input=new Scanner(System.in);
+		String str1=input.next(),str2=input.next();
+		int flag=0; 
+		int count=0;
+		for(int i=0;i<str1.length();i++)
+		{
+			String str3=str1.substring(i,str2.length()+i)	;
+			if(str2.equals(str3))
+			{
+				count=1;
+				System.out.println(i);
+				break;
+			}				
+		}
+		if(count==0)
+		{
+			System.out.print(-1);
+		}
+		input.close();
+	}
+}	
