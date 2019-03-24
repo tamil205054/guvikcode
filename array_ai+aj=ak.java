b09727678fab259154fb300b51c2312db5c0c217
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-24 00:30:30 -0700

    Create array_ai+aj=ak.java

---
+++ array_ai+aj=ak.java
@@ -0,0 +1,37 @@
+import  java.util.*;
+class Pair
+{
+	public static void main(String arg[])
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		int arrNum[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arrNum[i]=input.nextInt();
+		}
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			 int sum=0;
+			 for(int j=0;j<n;j++)
+			{
+				for(int k=0;k<n;k++)
+				{
+					if(i<j&&j<k&&i<k)
+					{
+					 sum=arrNum[i]+arrNum[j];
+						if(sum==arrNum[k])
+						{
+							//System.out.println(arrNum[i]+"+"+arrNum[i+1]+"="+sum);
+							count++;
+						}
+					}	 
+				}
+			}
+	
+			
+		}
+		System.out.print(count);
+	}
+}
