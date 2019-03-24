1d72798b52029b5d2388676825d3df4bb5334b37
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-24 00:08:39 -0700

    Create minbyes.java

---
+++ minbyes.java
@@ -0,0 +1,22 @@
+  
+import java.util.*;
+ 
+class Minbyes {
+
+	public static void main(String[] args) 
+	{
+		Scanner ip=new Scanner(System.in);
+		int n=ip.nextInt(); 
+		 if(n%2==0)
+			{			
+				System.out.print(0);
+		 	}
+		else
+			{
+			System.out.print(1);
+			}
+			
+		
+		ip.close();
+	}
+}
