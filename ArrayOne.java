476610aa0cd587bcbc556332169a28c603df3631
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-23 22:24:10 -0700

    Create ArrayOne.java

---
+++ ArrayOne.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Zero
+{
+	public static void main(String arg[])
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		int arrNum[]=new int[n];
+		List<Integer> result=new ArrayList<Integer>();
+		for(int i=0;i<n;i++)
+		{
+			arrNum[i]=input.nextInt();
+		}
+		int flag=0;
+		for(int i=n-1;i>=0;i--)
+		{
+			if(arrNum[i]==0)
+			{
+				flag++;	
+			}
+			if(flag==1 )
+			{
+				if(arrNum[i]==1)
+				{
+					result.add(arrNum[i]);
+				}	 
+			}
+			if(flag==2)
+			{
+				flag=0;
+				if((i-1)!=-1)
+				{
+				if(arrNum[i-1]==1)
+				{
+					result.add(arrNum[i-1]);
+				}
+				}
+			}
+			 
+		}
+		System.out.print(result);
+	}
+}
