da25b234ce28711c51cfcf6e7920ca67b4d75ee7
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-03-29 20:16:04 -0700

    Create prime_sum=n.java

---
+++ prime_sum=n.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class prime
+{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int two=2;
+        ArrayList<Integer> prime=new ArrayList<Integer>();
+        for(int i=0;i<=n;i++)
+        {
+            int count=0;
+            for(int j=1;j<=two;j++)
+            {
+                if(two%j==0)
+                {
+                    count++;
+                }
+            }
+            if(count==2)
+            {
+                prime.add(two);
+            }
+            two++;
+        }
+        int flag=0;
+        for(int i=0;i<=prime.size()/2;i++)
+        {
+            for(int j=prime.size()/2;j<prime.size();j++)
+            {
+                if(prime.get(i)+prime.get(j)==n)
+                {
+                    // System.out.println(prime.get(i)+"+"+prime.get(j)+"="+ (prime.get(i)+prime.get(j)));
+                    flag++;
+                }
+            }
+        }
+        System.out.println(flag);
+        in.close();
+    }
+}
