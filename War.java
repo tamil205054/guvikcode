71362d91ebdb88f936a4a3d2bc3388b45577a19a
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-04-04 23:59:49 -0700

    Create War.java

---
+++ War.java
@@ -0,0 +1,33 @@
+import java.util.*;	
+class War
+{
+	public static void main(String str[])
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=input.nextInt();
+		}
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				for(int k=0;k<n;k++)
+				{
+					if(i<j&&j<k&&i<k)
+					{
+						if(arr[i]>arr[j]&&arr[j]>arr[k]&&arr[i]>arr[k])
+						{	
+							count++;
+						}
+					}
+				}	
+			}
+		}
+		System.out.print(count);
+		input.close();
+	}
+}
