56cc935131f064b5d1fd2391ec04b6c5bb7d4162
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-04-10 20:51:03 -0700

    Create Oddprefix.java

---
+++ Oddprefix.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.lang.*;
+class  min
+{
+    public static void main(String[] args) 
+    {
+       Scanner input=new Scanner(System.in);
+       int n=input.nextInt();
+       int arr[]=new int[n];
+       int arr1[]=new int[n];
+       for(int i=0;i<n;i++)
+       {
+           arr[i]=input.nextInt();
+           
+       }
+       for(int i=0;i<n;i++)
+       {
+           int sum=0;
+        if(i==0)
+        {
+            arr[i]=arr[i];
+        }
+        
+            for(int j=0;j<=i;j++)
+            {
+                if(arr[i]%2==0)
+                {  
+            if(arr[j]%2==0)
+            {
+               sum=sum+arr[j];
+            }
+        }
+        else
+        {
+            sum=arr[i];
+            break;
+        }
+         
+           }
+           arr1[i]=sum;
+      
+       }
+       for(int i=0;i<n;i++)
+       {
+System.out.print(arr1[i]+" ");
+       }
+       input.close(); 
+    }
+}
