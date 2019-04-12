1875625eb2f35cc54b50fa5cb85440d511cecb3d
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-04-12 05:00:36 -0700

    Create Maxcel.java

---
+++ Maxcel.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class apartment
+{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int k=input.nextInt();
+        System.out.println(1+" "+(n-k));
+        input.close();
+    }
+}
