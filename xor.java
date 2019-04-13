e2fa0349aaa369f40109c8458340bd107ccb9300
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-04-12 23:07:45 -0700

    Create xor.java

---
+++ xor.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class xor
+{
+    public static void main(String[] args) 
+    {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int k=input.nextInt();
+        int num[]=new int[n];
+        List<Integer> result=new ArrayList<Integer>();
+        for(int i=0;i<n;i++)
+        {
+            num[i]=input.nextInt();
+        }
+        for(int i=0;i<k;i++)
+        {
+            int u=input.nextInt();
+            int v=input.nextInt();
+            int re=0;
+            for(int j=u-1;j<v-1;j++)
+            {
+                if(j==u-1)
+                {
+                    re=num[j]^num[j+1];
+                }
+                else
+                {
+                    re=re^num[j+1];
+                }
+            }
+            result.add(re);
+        }
+        for(int i=0;i<result.size();i++)
+        {
+            System.out.println(result.get(i));
+        }
+        input.close();
+    }
+}
