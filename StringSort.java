481762d7d7b871d650bdc921d6f33cd6935cebba
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-04-22 19:47:35 -0700

    Create StringSort.java

---
+++ StringSort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Str
+{
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		List<String>output=new ArrayList<String>();
+		for (int i=0;i<n;i++) 
+		{
+			String str=input.next().toLowerCase();
+			output.add(str);	
+		}
+		Collections.sort(output);
+		for(int i=0;i<output.size();i++)
+		{
+			System.out.println(output.get(i));
+		}
+	}
+}
