acf5b7e0d29e7604f99ea8b93e2945c0fb122f78
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-05-28 12:44:02 +0530

    Create counters.java

---
+++ counters.java
@@ -0,0 +1,11 @@
+ import java.util.*;
+ public class counters
+ {
+  public static void main(String str[])
+  {
+    Scanner input=new Scanner(System.in);
+    int n=input.nextInt();
+    System.out.print(n);
+    input.close();
+  }
+ }
