d09bc51a04ed09a4b7a205428f824734021ba5fa
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-05-29 18:01:13 +0530

    Update sine_angle.py

--- sine_angle.py
+++ sine_angle.py
@@ -1,7 +1,7 @@
 import math
 n=int(input())
-r=n*(math.pi/180)
-if r<1 and r>0:
-  print(round(math.sin(r),1))
+radious=n*(math.pi/180)
+if radious<1 and radious>0:
+  print(round(math.sin(radious),1))
 else:
-  print(round(math.sin(r)))
+  print(round(math.sin(radious)))
