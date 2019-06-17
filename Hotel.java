9e10ad2d4d3837fa7a0f01eaa54aca52db4b2b3b
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-06-16 23:50:23 -0700

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,34 +1,23 @@
 import java.util.*;
-class Hotel
+public class Hotel
 {
-	public static void main(String ar[])
-	{
-		Scanner input=new Scanner(System.in);
-		int n=input.nextInt();
-		int k=input.nextInt();
-		int items[]=new int[n];
-		
-		int annaBill=0;
-		for(int i=0;i<n;i++)
-		{
-			items[i]=input.nextInt();
-			 
-			if(i!=k)
-			{	
-				annaBill=annaBill+items[i];
-			}
-		}
-		int totalBill=input.nextInt();
-		int diff=totalBill-(annaBill/2);
-		if(diff==0)
-		{
-			System.out.print("Bon Appetit");
-		}
-		else
-		{
-			System.out.print(diff);
-		}
-		
-		input.close();
-	}	 
-} 
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int item=in.nextInt();
+    int[] arr=new int[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        arr[i]=in.nextInt();
+        sum+=arr[i];
+    }
+    int paid=in.nextInt();
+    sum=sum-arr[item];
+    sum=sum/2;
+    if(paid-sum>0){
+        System.out.print(paid-sum);
+    }else{
+        System.out.print("Bon Appetit");
+    }
+  }
+}
