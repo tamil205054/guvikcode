1d27bc782735bd42b6d3f4844fc35b9e8f818679
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-06-16 23:30:56 -0700

    Update Maxcel.java

--- Maxcel.java
+++ Maxcel.java
@@ -2,10 +2,12 @@ import java.util.*;
 class apartment
 {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        int n=input.nextInt();
-        int k=input.nextInt();
-        System.out.println(1+" "+(n-k));
-        input.close();
+      Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int k=in.nextInt();
+    int ava=n-k;
+    int pos=2*k;
+    int max=Math.min(ava,pos);
+    System.out.println(1+" "+max);
     }
 }
