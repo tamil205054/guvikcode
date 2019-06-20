0aebc555dc247c29e808e1126c2bd1120cbb57ec
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-06-19 22:05:02 -0700

    Create Road_Damage.java

---
+++ Road_Damage.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Road_Damage
+{
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		int k=input.nextInt();
+		String str=input.next();
+		str=str.substring(0,n);
+	  	int count=0;
+	  	int start=0;
+	  	int end=0;
+	  	for(int i=0;i<str.length();i++)
+	  	{
+	  		if(str.charAt(i)=='*')
+	  		{
+	  			start=i;
+	  			i=str.length();
+	  		}
+	  	}
+	  	for (int i=str.length()-1;i>=0;i--) 
+	  	{
+	  		if(str.charAt(i)=='*')
+	  		{
+	  			end=i;
+	  			i=-1;
+	  		}
+	  	}
+	  	if(start!=0)
+	  	{
+	  		start=start-1;
+	  	}
+	  	else
+	  	{
+	  		start=start+1;
+	  	}
+	  	int total=start+end;
+	  	while(total>0)
+	  	{
+	  		total=total-k;
+	  		count++;
+	  	}
+	  	System.out.println(count); 
+		input.close();	
+	}
+}
