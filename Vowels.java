7d2a763c321861a368c110747231b290356e7f84
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-06-21 03:34:02 -0700

    Create Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Vowels {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String s=in.next();
+    s=s.toLowerCase();
+    int c=0;
+    int t=0;
+    for(int i=0;i<s.length()-1;i+=2){
+      if((!(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'))&&(s.charAt(i+1)=='a'||s.charAt(i+1)=='e'||s.charAt(i+1)=='i'||s.charAt(i+1)=='o'||s.charAt(i+1)=='u')){
+        t+=2;
+        c=Math.max(c,t);
+      }
+      else{
+        t=0;
+      }
+    }
+
+    System.out.print(c);
+
+      
+  }
+}
