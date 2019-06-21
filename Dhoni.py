7cd30f40d27c556bf6a73cb1512148411a069fe2
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-06-21 00:20:11 -0700

    Update Dhoni.py

--- Dhoni.py
+++ Dhoni.py
@@ -1,15 +1,15 @@
 #ry
 n = int(input())
-b = []
+list_b = []
 a = n//2 + n
 for i in range(1,n+1):
   if i%2==0:
-    b.append(i)
+    list_b.append(i)
 for i in range(1,n+1):
   if i%2!=0:
-    b.append(i)
+    list_b.append(i)
 for i in range(1,n+1):
   if i%2==0:
-    b.append(i)
+    list_b.append(i)
 print(a)
-print(*b)
+print(*list_b)
