195d9661d009f8eae822a4912174016f5d8a5f26
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-06-23 04:40:48 -0700

    Update MatrixAvg.java

--- MatrixAvg.java
+++ MatrixAvg.java
@@ -1,29 +1,26 @@
 import java.util.*;
-class Matrix{
-
-    public static void main(String[] args) {
+ public class MatrixAvg{ 
+	  
+	  
+	public static void main (String[] args) 
+	{ 
 		Scanner input=new Scanner(System.in);
-		int n=input.nextInt();
-        int arr[][] =new int[n][n];
-        int total = 0;
+		int n = input.nextInt();
+		float sum=0.000000f; 
+		float arr[][]=new float[n][n];
+		 for(int i=0;i<n;i++)
+		 {
+		 	for (int j=0;j<n;j++ ) 
+		 	{
+		 		arr[i][j]=input.nextFloat();
+		 		sum+=arr[i][j];	
+		 	}
 
-        for(int i=0; i<n; i++)
-		{
-			for(int j=0;j<n;j++)
-			{
-				 arr[i][j]=input.nextInt();
-			}
-        }
-		
-        for(int i=0; i<n; i++)
-		{
-			for(int j=0;j<n;j++)
-			{
-				 total+=arr[i][j];
-			}
-        }
+		 }
+		 float avg=sum/(n*n);
+		 System.out.printf("%.6f",avg);
+		input.close();
+	} 
+} 
+
  
-       float average = total / (n*n);
-        System.out.format("%.6f",average);
-    }
-}
