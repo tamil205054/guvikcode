51151f871bd2212858ed222516e306f2e5b8a1ee
tamil205054 <47376936+tamil205054@users.noreply.github.com>
2019-06-29 13:14:23 +0530

    Create BdayCake.java

---
+++ BdayCake.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class BdayCake
+{
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		int m=input.nextInt();
+		int n=input.nextInt();
+		String cake[][]=new String[m][n];
+		for (int i=0;i<m;i++) 
+		{
+			for(int j=0;j<n;j++)
+			{
+			cake[i][j]=input.next().toUpperCase();	
+			}
+				
+		}
+		int count=0;
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<n-1;j++)
+			{
+				 	if(cake[i][j].equals(cake[i][j+1]))
+				 	{
+				 		 
+				 		if(cake[i][j].equals("R"))
+				 		{
+				 			count+=5;
+				 		}
+				 		else
+				 		{
+				 			count+=3;
+				 		}
+				 		j++;
+				 	}
+			}
+		}
+		System.out.print(count);
+	}
+}
